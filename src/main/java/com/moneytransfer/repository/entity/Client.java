885e86c5464c392a7025846b384cca1c3deda7e7
Frankyn Vieira <frankfhurt@gmail.com>
2019-08-28 21:42:17 -0300

    Created equals and hashCode methods for Account and Client

--- src/main/java/com/moneytransfer/repository/entity/Client.java
+++ src/main/java/com/moneytransfer/repository/entity/Client.java
@@ -49,4 +49,41 @@ public class Client {
 		this.account = account;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((document == null) ? 0 : document.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		if (document == null) {
+			if (other.document != null)
+				return false;
+		} else if (!document.equals(other.document))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
 }
